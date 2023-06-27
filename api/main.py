2a6e8f2f43e3a48a27d6b8a013e1ee241346d7d0
gengen <gengen0630@gmail.com>
2023-06-27 17:16:08 +0900

    [add] download file button

--- api/main.py
+++ api/main.py
@@ -44,7 +44,7 @@ async def solve(request: Request, uploaded_files: list[UploadFile] = None):
     """ メインページのエンドポイント. アップロードされたデータを保存し、数理最適化を行う. """
     # データがアップロードされていなければトップページにリダイレクト
     if not uploaded_files[0].filename:
-        return RedirectResponse(url='http://localhost:8000/')
+        return RedirectResponse(url='/')
     else:
         # アップロードされたファイルを保存
         save_dir = BASE_DIR.parent.joinpath('data', 'uploaded')
@@ -53,16 +53,13 @@ async def solve(request: Request, uploaded_files: list[UploadFile] = None):
             with open(save_dir.joinpath(uploaded_file.filename), 'wb+') as f:
                 shutil.copyfileobj(uploaded_file.file, f)
         
-        # TODO: 最適化を実行
+        # TODO: 最適化を実行する処理を実装
         solution_html = '<p>This is a test</p>'
         
         query_dict = {
         'request': request,
         'solution_html': solution_html
         }
-        
-        # TODO: index.htmlに最適化後の時間割を表示(index.htmlに追記)
-        # TODO: index.htmlに/timetableへのリンクを作成(結果をフィルタリングボタンでリンクを追加)
         
         return templates.TemplateResponse('index.html', query_dict)
 
@@ -103,8 +100,6 @@ async def view_result(request: Request,
     
     df_html = df.sort_values(by=['時限']).to_html(index=False)
     
-    # TODO: よくみる時間割の表形式に並べて表示、ダウンロードボタンでcsvまたはexcelで保存できるように
-    
     query_dict = {
         'request': request,
         'df_html': df_html,
@@ -117,7 +112,9 @@ async def view_result(request: Request,
     return templates.TemplateResponse('result.html', query_dict)
 
 @app.get('/download/{name}', response_class=FileResponse)
-async def download_file(name: str):
+async def download_file(name: str = None):
     """ ファイルをダウンロードするエンドポイント """
+    if not name:
+        name = 'toy/result.csv'
     path = BASE_DIR.parent.joinpath(f'outputs/{name}')
     return path
